95b8843f9d7904b4b484ab222371acdabf3f8834
NickPustovoy <pustovoy.kolya@mail.ru>
2021-12-21 19:59:03 +0300

    Summery

---
+++ DZ5_IVT-21B_Nick P._Var2/DZ5_IVT-21B_Nick P._Var2/Address.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DZ5_IVT_21B_Nick_P._Var2
+{
+    class Address
+    {
+        public string Country;
+        string Area;
+        public string City;
+        string Street;
+        int Building;
+        int PostalСode;
+        public Address(string Area, string City, string Street, int Building, int PostalСode)
+        :
+            this("Россия", Area, City, Street, Building, PostalСode)
+        { 
+        }
+        public Address(string Country, string Area, string City, string Street, int Building, int PostalСode)
+        {
+            this.Country = Country;
+            this.Area = Area;
+            this.City = City;
+            this.Street = Street;
+            this.Building = Building;
+            this.PostalСode = PostalСode;
+            if(Country != "Россия" && 
+                Country != "Украина" && 
+                Country !=  "Казахстан" && 
+                Country != "Беларусь")
+            {
+                throw new Exception("Неверно указана страна");
+            }
+            if(Country == "Россия" && (PostalСode < 100000 || PostalСode > 1000000))
+            {
+                throw new Exception("Почтовый номер должен состоять из 6-ти цифр");
+            }
+        }
+        public Address(string Area, string City, int Building, int PostalСode)
+        :
+            this("Россия", Area, City, "" , Building, PostalСode)
+        {
+        }
+        public override string ToString()
+        {
+            if (Street.Length == 0)
+            {
+                return $"Страна: {Country}, Область: {Area}, Город: {City}, Дом: {Building}, Почтовый Индекс: {PostalСode}";
+            }
+            return $"Страна: {Country}, Область: {Area}, Город: {City}, Улица: {Street}, Дом: {Building}, Почтовый Индекс: {PostalСode}";
+        }
+    }
+    
+}
---
+++ DZ5_IVT-21B_Nick P._Var2/DZ5_IVT-21B_Nick P._Var2/MainWindow.xaml.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DZ5_IVT_21B_Nick_P._Var2
+{
+    /// Задание: 
+    /// 1) Добавить поле для города (Кнопка)
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Address address = new Address("Москва", "Зеленоград", "Площадь Шокина", 1, 124498);
+            Address address2 = new Address("Украина", "Киевская область", "Киев", "Владимирская", 60, 01033);
+            Address address3 = new Address("Москва", "Зеленоград", 713, 124498);
+            AddressList.Items.Add(address);
+            AddressList.Items.Add(address2);
+            AddressList.Items.Add(address3);
+
+        }
+
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Address ad = new Address(Area.Text, City.Text, int.Parse(Building.Text), int.Parse(PostalCode.Text));
+                AddressList.Items.Add(ad);
+            }
+            catch (FormatException ead)
+            {
+                MessageBox.Show("Проверьте правильность заполнения Номера Дома или Почтового Индекса");
+            }
+            catch (Exception ead)
+            {
+                MessageBox.Show(ead.Message);
+            }
+            Country.Text = "";
+            Area.Text = "";
+            City.Text = "";
+            Street.Text = "";
+            Building.Text = "";
+            PostalCode.Text = "";
+        }
+
+        private void Sort_Click(object sender, RoutedEventArgs e)
+        {
+            List<Address> addresses = new List<Address>();
+            foreach (var adr in AddressList.Items)
+            {
+                if ((adr as Address).Country == "Россия")
+                {
+                    addresses.Add(adr as Address);
+                }
+            }
+            AddressList.Items.Clear();
+            foreach (var adr in addresses)
+            {
+                AddressList.Items.Add(adr);
+            }
+        }
+        private void Window_Loaded_1(object sender, RoutedEventArgs e)
+        {
+            Country.Items.Add("Россия");
+            Country.Items.Add("Украина");
+            Country.Items.Add("Казахстан");
+            Country.Items.Add("Беларусь");
+        }
+
+        private void Srt_Click(object sender, RoutedEventArgs e)
+        {
+            
+            Address p;
+            List<Address> addresses = new List<Address>();
+            foreach(var obj in AddressList.Items)
+            {
+                addresses.Add(obj as Address);
+            }
+            AddressList.Items.Clear();
+            for (int j = 0; j < addresses.Count - 1; j++)
+            {
+                for (int i = 0; i < addresses.Count - 1; i++)
+                {
+                    if(addresses[i].Country.CompareTo(addresses[i + 1].Country) > 0)
+                    {
+                        p = addresses[i];
+                        addresses[i] = addresses[i + 1];
+                        addresses[i + 1] = p;
+                    }
+                }
+            }
+            foreach (var obj in addresses)
+            {
+                AddressList.Items.Add(obj as Address);
+            }
+        }
+
+        private void Nfilt_Click(object sender, RoutedEventArgs e)
+        {
+            Address p;
+            List<Address> addresses = new List<Address>();
+            foreach (var obj in AddressList.Items)
+            {
+                if((obj as Address).City == Ctname.Text)
+                {
+                    addresses.Add(obj as Address);
+                }               
+            }
+            AddressList.Items.Clear();
+            foreach (var obj in addresses)
+            {
+                AddressList.Items.Add(obj as Address);
+            }
+        }
+    }
+}
